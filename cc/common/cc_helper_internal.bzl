bazelbuild/rules_cc

commit b81a4f4808d05c1d9d62be90d6188b8acdc9794f

Enable C++ symbols using compatiblity proxy
PiperOrigin-RevId: 823465606
Change-Id: I4f9654e6081d01a7a8c3bb4f2357de49d20a0ede

cc/BUILD
@@ -55,8 +55,7 @@ filegroup(
     srcs = glob([
         "**/*.bzl",
     ]) + [
-        "//cc/private/rules_impl:bzl_srcs",
-        "//cc/private/toolchain:bzl_srcs",
+        "//cc/private:bzl_srcs",
     ],
     visibility = ["//visibility:public"],
 )
@@ -67,8 +66,7 @@ filegroup(
         "**/*.bzl",
         "**/BUILD",
     ]) + [
-        "//cc/private/rules_impl:srcs",
-        "//cc/private/toolchain:srcs",
+        "//cc/private:srcs",
     ],
     visibility = ["//visibility:public"],
 )

cc/extensions.bzl
@@ -35,10 +35,11 @@ def _compatibility_proxy_repo_impl(rctx):
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 bzl_library(
   name = "proxy_bzl",
-  srcs = ["proxy.bzl"],
+  srcs = ["proxy.bzl", "symbols.bzl"],
   deps = [
     "@rules_cc//cc/private/rules_impl:core_rules",
     "@rules_cc//cc/private/rules_impl:toolchain_rules",
+    "@rules_cc//cc/private:cc_common",
   ],
   visibility = ["@rules_cc//cc:__subpackages__"],
 )
@@ -80,6 +81,20 @@ cc_toolchain_alias = _cc_toolchain_alias
 CcSharedLibraryInfo = _CcSharedLibraryInfo
             """,
         )
+        rctx.file(
+            "symbols.bzl",
+            """
+load("@rules_cc//cc/private:cc_common.bzl", _cc_common = "cc_common")
+load("@rules_cc//cc/private:cc_info.bzl", _CcInfo = "CcInfo")
+load("@rules_cc//cc/private/toolchain_config:cc_toolchain_config_info.bzl", _CcToolchainConfigInfo = "CcToolchainConfigInfo")
+load("@rules_cc//cc/private:debug_package_info.bzl", _DebugPackageInfo = "DebugPackageInfo")
+
+cc_common = _cc_common
+CcInfo = _CcInfo
+DebugPackageInfo = _DebugPackageInfo
+CcToolchainConfigInfo = _CcToolchainConfigInfo
+            """,
+        )
     else:
         rctx.file(
             "BUILD",
@@ -87,7 +102,7 @@ CcSharedLibraryInfo = _CcSharedLibraryInfo
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 bzl_library(
   name = "proxy_bzl",
-  srcs = ["proxy.bzl"],
+  srcs = ["proxy.bzl", "symbols.bzl"],
   deps = ["@rules_cc//cc/private/rules_impl:native_bzl"],
   visibility = ["@rules_cc//cc:__subpackages__"],
 )
@@ -113,6 +128,21 @@ cc_toolchain = native.cc_toolchain
 cc_toolchain_alias = native.cc_toolchain_alias
 
 CcSharedLibraryInfo = NativeCcSharedLibraryInfo
+            """,
+        )
+        rctx.file(
+            "symbols.bzl",
+            """
+load("@rules_cc//cc/private/rules_impl:native.bzl", "native_cc_common")
+load("@rules_cc//cc/private/rules_impl:native.bzl", "NativeCcInfo")
+load("@rules_cc//cc/private/rules_impl:native.bzl", "NativeDebugPackageInfo")
+load("@rules_cc//cc/private/rules_impl:native.bzl", "NativeCcToolchainConfigInfo")
+load("@rules_cc//cc/private/rules_impl:native.bzl", "NativeCcSharedLibraryInfo")
+
+cc_common = native_cc_common
+CcInfo = NativeCcInfo
+DebugPackageInfo = NativeDebugPackageInfo
+CcToolchainConfigInfo = NativeCcToolchainConfigInfo
             """,
         )
 

cc/private/BUILD
@@ -15,6 +15,20 @@
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 
 filegroup(
+    name = "bzl_srcs",
+    srcs = glob([
+        "**/*.bzl",
+    ]) + [
+        "//cc/private/compile:bzl_srcs",
+        "//cc/private/link:bzl_srcs",
+        "//cc/private/rules_impl:bzl_srcs",
+        "//cc/private/toolchain:bzl_srcs",
+        "//cc/private/toolchain_config:bzl_srcs",
+    ],
+    visibility = ["//visibility:public"],
+)
+
+filegroup(
     name = "srcs",
     srcs = glob([
         "**/BUILD",
@@ -32,6 +46,32 @@ filegroup(
 bzl_library(
     name = "paths_bzl",
     srcs = ["paths.bzl"],
+    visibility = ["//cc:__subpackages__"],
+)
+
+bzl_library(
+    name = "cc_common",
+    srcs = ["cc_common.bzl"],
+    visibility = [
+        "//cc:__subpackages__",
+        "@cc_compatibility_proxy//:__pkg__",
+    ],
+    deps = [
+        ":cc_info_bzl",
+        ":cc_internal_bzl",
+        ":cc_launcher_info_bzl",
+        ":cc_shared_library_hint_info_bzl",
+        ":debug_package_info_bzl",
+        "//cc/private/compile:compile_bzl",
+        "//cc/private/link:link_bzl",
+        "//cc/private/rules_impl:native_bzl",
+        "//cc/private/toolchain_config:toolchain_config_bzl",
+    ],
+)
+
+bzl_library(
+    name = "cc_info_bzl",
+    srcs = ["cc_info.bzl"],
     visibility = ["//cc:__subpackages__"],
 )
 

cc/private/compile/BUILD
@@ -28,6 +28,14 @@ bzl_library(
 )
 
 filegroup(
+    name = "bzl_srcs",
+    srcs = glob([
+        "**/*.bzl",
+    ]),
+    visibility = ["//visibility:public"],
+)
+
+filegroup(
     name = "srcs",
     srcs = glob([
         "**/*.bzl",

cc/private/link/BUILD
@@ -29,6 +29,14 @@ bzl_library(
 )
 
 filegroup(
+    name = "bzl_srcs",
+    srcs = glob([
+        "**/*.bzl",
+    ]),
+    visibility = ["//visibility:public"],
+)
+
+filegroup(
     name = "srcs",
     srcs = glob([
         "**/*.bzl",

cc/private/toolchain_config/BUILD
@@ -28,6 +28,14 @@ bzl_library(
 )
 
 filegroup(
+    name = "bzl_srcs",
+    srcs = glob([
+        "**/*.bzl",
+    ]),
+    visibility = ["//visibility:public"],
+)
+
+filegroup(
     name = "srcs",
     srcs = glob([
         "**/*.bzl",

cc/common/cc_common.bzl
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """cc_common module"""
 
-load("//cc/private/rules_impl:native.bzl", "native_cc_common")
+load("@cc_compatibility_proxy//:symbols.bzl", _cc_common = "cc_common")
 
-cc_common = native_cc_common
+cc_common = _cc_common

cc/common/cc_info.bzl
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """CcInfo"""
 
-load("//cc/private/rules_impl:native.bzl", "NativeCcInfo")
+load("@cc_compatibility_proxy//:symbols.bzl", _CcInfo = "CcInfo")
 
-CcInfo = NativeCcInfo
+CcInfo = _CcInfo

commit e3b8243fca77ccc83ccae89f38546d7e87cfd5d1

Fix bzl_library deps for `//cc/common`
Broken by https://github.com/bazelbuild/rules_cc/commit/b81a4f4808d05c1d9d62be90d6188b8acdc9794f

PiperOrigin-RevId: 827420944
Change-Id: I7fe78f7f718e8db281b43ab8a0d6c07f08706539

cc/BUILD
@@ -118,6 +118,7 @@ bzl_library(
     deps = [
         ":core_rules",
         "//cc/common",
+        "//cc/common:cc_helper_bzl",
         "//cc/private/rules_impl:failing_cc_proto_library_bzl",
         "//cc/toolchains:toolchain_rules",
     ],

cc/common/BUILD
@@ -13,15 +13,28 @@
 # limitations under the License.
 
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
+load("//cc:starlark_doc_extract_helper.bzl", "starlark_doc_extract_helper")
 
 bzl_library(
     name = "common",
-    srcs = glob(["*.bzl"]),
+    srcs = glob(
+        ["*.bzl"],
+        exclude = [
+            "cc_helper.bzl",
+            "cc_debug_helper.bzl",
+            "visibility.bzl",
+            "semantics.bzl",
+            "cc_helper_internal.bzl",
+            "cc_shared_library_info.bzl",
+        ],
+    ),
     visibility = ["//visibility:public"],
     deps = [
         "//cc/private:cc_internal_bzl",
         "//cc/private:paths_bzl",
         "//cc/private/rules_impl:native_bzl",
+        "@bazel_skylib//lib:paths",
+        "@cc_compatibility_proxy//:symbols_bzl",
     ],
 )
 
@@ -33,6 +46,32 @@ bzl_library(
         ":visibility_bzl",
         "//cc/private:paths_bzl",
         "//cc/private/rules_impl:objc_common",
+    ],
+)
+
+bzl_library(
+    name = "cc_shared_library_info_bzl",
+    srcs = ["cc_shared_library_info.bzl"],
+    visibility = ["//cc:__subpackages__"],
+    deps = [
+        "@cc_compatibility_proxy//:proxy_bzl",
+    ],
+)
+
+bzl_library(
+    name = "semantics_bzl",
+    srcs = ["semantics.bzl"],
+    visibility = ["//cc:__subpackages__"],
+)
+
+bzl_library(
+    name = "cc_helper_internal_bzl",
+    srcs = ["cc_helper_internal.bzl"],
+    visibility = ["//cc:__subpackages__"],
+    deps = [
+        "//cc/private:cc_internal_bzl",
+        "//cc/private:paths_bzl",
+        "@bazel_skylib//lib:paths",
     ],
 )
 
@@ -61,3 +100,23 @@ filegroup(
     ]),
     visibility = ["//visibility:public"],
 )
+
+# unused targets to test that we have the right bzl_library graph
+[starlark_doc_extract_helper(
+    name = f + "_doc_extract",
+    src = f,
+    visibility = ["//visibility:private"],
+    deps = [
+        ":common",
+    ],
+) for f in glob(
+    ["*.bzl"],
+    exclude = [
+        "cc_helper.bzl",
+        "cc_debug_helper.bzl",
+        "visibility.bzl",
+        "semantics.bzl",
+        "cc_helper_internal.bzl",
+        "cc_shared_library_info.bzl",
+    ],
+)]

cc/common/cc_helper_internal.bzl
@@ -285,7 +285,8 @@ def is_stamping_enabled(ctx):
         ctx: The rule context.
 
     Returns:
-    (int): 1: Always stamp the build information into the binary, even in [--nostamp][stamp] builds.
+        (int) Possible values are:
+        1: Always stamp the build information into the binary, even in [--nostamp][stamp] builds.
         This setting should be avoided, since it potentially kills remote caching for the binary and
         any downstream actions that depend on it.
         0: Always replace build information by constant values. This gives good build result caching.
@@ -350,6 +351,7 @@ def root_relative_path(file):
 
     Args:
         file: (File) The file to get the root-relative path for.
+
     Returns:
         (str) The root-relative path of the file.
     """

cc/extensions.bzl
@@ -35,11 +35,20 @@ def _compatibility_proxy_repo_impl(rctx):
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 bzl_library(
   name = "proxy_bzl",
-  srcs = ["proxy.bzl", "symbols.bzl"],
+  srcs = ["proxy.bzl"],
   deps = [
     "@rules_cc//cc/private/rules_impl:core_rules",
     "@rules_cc//cc/private/rules_impl:toolchain_rules",
     "@rules_cc//cc/private:cc_common",
+  ],
+  visibility = ["@rules_cc//cc:__subpackages__"],
+)
+bzl_library(
+  name = "symbols_bzl",
+  srcs = ["symbols.bzl"],
+  deps = [
+    "@rules_cc//cc/private:cc_common",
+    "@rules_cc//cc/private/toolchain_config:toolchain_config_bzl",
   ],
   visibility = ["@rules_cc//cc:__subpackages__"],
 )
@@ -105,7 +114,13 @@ new_objc_provider = _ObjcInfo
 load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
 bzl_library(
   name = "proxy_bzl",
-  srcs = ["proxy.bzl", "symbols.bzl"],
+  srcs = ["proxy.bzl"],
+  deps = ["@rules_cc//cc/private/rules_impl:native_bzl"],
+  visibility = ["@rules_cc//cc:__subpackages__"],
+)
+bzl_library(
+  name = "symbols_bzl",
+  srcs = ["symbols.bzl"],
   deps = ["@rules_cc//cc/private/rules_impl:native_bzl"],
   visibility = ["@rules_cc//cc:__subpackages__"],
 )

cc/private/BUILD
@@ -65,6 +65,7 @@ bzl_library(
         ":objc_info_bzl",
         "//cc/private/compile:compile_bzl",
         "//cc/private/link:link_bzl",
+        "//cc/private/rules_impl:cc_toolchain_info_bzl",
         "//cc/private/rules_impl:native_bzl",
         "//cc/private/toolchain_config:toolchain_config_bzl",
     ],
@@ -98,7 +99,7 @@ bzl_library(
     name = "cc_launcher_info_bzl",
     srcs = ["cc_launcher_info.bzl"],
     visibility = ["//cc:__subpackages__"],
-    deps = ["//cc/common"],
+    deps = ["//cc/common:cc_helper_internal_bzl"],
 )
 
 bzl_library(

cc/private/compile/BUILD
@@ -20,7 +20,7 @@ bzl_library(
     visibility = ["//visibility:public"],
     deps = [
         "//cc:action_names_bzl",
-        "//cc/common",
+        "//cc/common:semantics_bzl",
         "//cc/private:cc_internal_bzl",
         "//cc/private/rules_impl:native_bzl",
         "@bazel_skylib//lib:paths",

cc/private/link/BUILD
@@ -20,7 +20,7 @@ bzl_library(
     visibility = ["//visibility:public"],
     deps = [
         "//cc:action_names_bzl",
-        "//cc/common",
+        "//cc/common:semantics_bzl",
         "//cc/private:cc_internal_bzl",
         "//cc/private/compile:compile_bzl",
         "//cc/private/rules_impl:native_bzl",

cc/private/rules_impl/BUILD
@@ -56,6 +56,7 @@ bzl_library(
         "//cc:action_names_bzl",
         "//cc:find_cc_toolchain_bzl",
         "//cc/common",
+        "//cc/common:cc_debug_helper_bzl",
         "@bazel_skylib//lib:paths",
         "@com_google_protobuf//bazel/common:proto_info_bzl",
     ],
@@ -106,6 +107,15 @@ bzl_library(
 bzl_library(
     name = "native_bzl",
     srcs = ["native.bzl"],
+    visibility = [
+        "//cc:__subpackages__",
+        "@cc_compatibility_proxy//:__pkg__",
+    ],
+)
+
+bzl_library(
+    name = "cc_toolchain_info_bzl",
+    srcs = ["cc_toolchain_info.bzl"],
     visibility = [
         "//cc:__subpackages__",
         "@cc_compatibility_proxy//:__pkg__",

cc/private/toolchain_config/BUILD
@@ -21,7 +21,7 @@ bzl_library(
     deps = [
         "//cc:action_names_bzl",
         "//cc:cc_toolchain_config_lib_bzl",
-        "//cc/common",
+        "//cc/common:semantics_bzl",
         "//cc/private:cc_internal_bzl",
         "//cc/private/rules_impl:native_bzl",
         "@bazel_skylib//lib:paths",

cc/starlark_doc_extract_helper.bzl
@@ -1,6 +1,6 @@
 """Indirection to avoid breaking on Bazel 6"""
 
-visibility("private")
+visibility("//cc/...")
 
 def starlark_doc_extract_helper(**kwargs):
     """Creates a starlark_doc_extract target if the native rule is available