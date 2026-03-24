bazelbuild/rules_cc

commit db1d07d78987ac9e996d8c0ca1f1b4b87554ee51

Add requires_not_none support for cc_args.env
Copybara Import from https://github.com/bazelbuild/rules_cc/pull/484

BEGIN_PUBLIC
Add requires_not_none support for cc_args.env (#484)

Since this commit
https://github.com/bazelbuild/rules_cc/commit/5a8cab742c3868ae5fc813745c8677f676444cf1
bazel has supported expand_if_available for env_sets. This is useful
when it's easier in the tool being run to accept optional arguments via
environment variables vs potentially double parsing and filtering
arguments.

For example in the case of parse_headers actions you either have to
parse the command line (including optional response files) in order to
find the value passed to `-o`, or you can organize it so you can read
it from the environment instead with this.

Closes #484
END_PUBLIC

COPYBARA_INTEGRATE_REVIEW=https://github.com/bazelbuild/rules_cc/pull/484 from keith:ks/add-requires_not_none-support-for-cc_args.env 75923309c10be9151c757dcb85cb57878afd1326
PiperOrigin-RevId: 804213976
Change-Id: I31f95d427ddc3e6cd64cf548e7b65db7a1672920

cc/toolchains/args.bzl
@@ -14,7 +14,7 @@
 """All providers for rule-based bazel toolchain config."""
 
 load("@bazel_skylib//rules/directory:providers.bzl", "DirectoryInfo")
-load("//cc/toolchains/impl:args_utils.bzl", "validate_nested_args")
+load("//cc/toolchains/impl:args_utils.bzl", "validate_env_variables", "validate_nested_args")
 load(
     "//cc/toolchains/impl:collect.bzl",
     "collect_action_types",
@@ -33,7 +33,9 @@ load(
     "ArgsInfo",
     "ArgsListInfo",
     "BuiltinVariablesInfo",
+    "EnvInfo",
     "FeatureConstraintInfo",
+    "VariableInfo",
 )
 
 visibility("public")
@@ -68,12 +70,24 @@ def _cc_args_impl(ctx):
 
     requires = collect_provider(ctx.attr.requires_any_of, FeatureConstraintInfo)
 
+    env = EnvInfo(
+        label = ctx.label,
+        entries = formatted_env,
+        requires_not_none = ctx.attr.requires_not_none[VariableInfo].name if ctx.attr.requires_not_none else None,
+    )
+    validate_env_variables(
+        actions = actions.to_list(),
+        env = env,
+        variables = ctx.attr._variables[BuiltinVariablesInfo].variables,
+        used_format_vars = used_format_vars,
+    )
+
     args = ArgsInfo(
         label = ctx.label,
         actions = actions,
         requires_any_of = tuple(requires),
         nested = nested,
-        env = formatted_env,
+        env = env,
         files = files,
         allowlist_include_directories = depset(
             direct = [d[DirectoryInfo] for d in ctx.attr.allowlist_include_directories],

tests/rule_based_toolchain/args/BUILD
@@ -2,6 +2,7 @@ load("@rules_testing//lib:util.bzl", "util")
 load("//cc/toolchains:args.bzl", "cc_args")
 load("//cc/toolchains/impl:variables.bzl", "cc_variable", "types")
 load("//tests/rule_based_toolchain:analysis_test_suite.bzl", "analysis_test_suite")
+load("//tests/rule_based_toolchain:testing_rules.bzl", "expect_failure_test")
 load(":args_test.bzl", "TARGETS", "TESTS")
 
 cc_variable(
@@ -37,6 +38,15 @@ util.helper_target(
 
 util.helper_target(
     cc_args,
+    name = "env_only_requires",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"BAR": "{dependency_file}"},
+    format = {"dependency_file": "//cc/toolchains/variables:dependency_file"},
+    requires_not_none = "//cc/toolchains/variables:dependency_file",
+)
+
+util.helper_target(
+    cc_args,
     name = "iterate_over_optional",
     actions = ["//cc/toolchains/actions:compile_actions"],
     args = ["{user_compile_flags}"],
@@ -51,6 +61,68 @@ util.helper_target(
     actions = ["//tests/rule_based_toolchain/actions:all_compile"],
     allowlist_include_directories = ["//tests/rule_based_toolchain/testdata:directory"],
     args = ["--secret-builtin-include-dir"],
+)
+
+util.helper_target(
+    cc_args,
+    name = "good_env_format",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"FOO": "{gcov_gcno_file}"},
+    format = {"gcov_gcno_file": "//cc/toolchains/variables:gcov_gcno_file"},
+)
+
+util.helper_target(
+    cc_args,
+    name = "good_env_format_optional",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"FOO": "{dependency_file}"},
+    format = {"dependency_file": "//cc/toolchains/variables:dependency_file"},
+    requires_not_none = "//cc/toolchains/variables:dependency_file",
+)
+
+util.helper_target(
+    cc_args,
+    name = "bad_env_format_list",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"FOO": "{preprocessor_defines}"},
+    format = {"preprocessor_defines": "//cc/toolchains/variables:preprocessor_defines"},
+    tags = ["manual"],
+)
+
+expect_failure_test(
+    name = "bad_env_format_list_test",
+    failure_message = "format only works on string, file, or directory type variables, but preprocessor_defines has type List[string]",
+    target = ":bad_env_format_list",
+)
+
+util.helper_target(
+    cc_args,
+    name = "bad_env_format_optional",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"FOO": "{user_compile_flags}"},
+    format = {"user_compile_flags": "//cc/toolchains/variables:user_compile_flags"},
+    tags = ["manual"],
+)
+
+expect_failure_test(
+    name = "bad_env_format_optional_test",
+    failure_message = "format only works on string, file, or directory type variables, but user_compile_flags has type List[string]",
+    target = ":bad_env_format_optional",
+)
+
+util.helper_target(
+    cc_args,
+    name = "bad_env_format_wrong_action",
+    actions = ["//cc/toolchains/actions:compile_actions"],
+    env = {"FOO": "{runtime_solib_name}"},
+    format = {"runtime_solib_name": "//cc/toolchains/variables:runtime_solib_name"},
+    tags = ["manual"],
+)
+
+expect_failure_test(
+    name = "bad_env_format_wrong_action_test",
+    failure_message = "runtime_solib_name is inaccessible from the action",
+    target = ":bad_env_format_wrong_action",
 )
 
 analysis_test_suite(

tests/rule_based_toolchain/args/args_test.bzl
@@ -62,7 +62,7 @@ def _simple_test(env, targets):
         targets.c_compile.label,
         targets.cpp_compile.label,
     ])
-    simple.env().contains_exactly({"BAR": "bar"})
+    simple.env().entries().contains_exactly({"BAR": "bar"})
     simple.files().contains_exactly(_SIMPLE_FILES)
 
     c_compile = env.expect.that_target(targets.simple).provider(ArgsListInfo).by_action().get(
@@ -91,7 +91,7 @@ def _env_only_test(env, targets):
         targets.c_compile.label,
         targets.cpp_compile.label,
     ])
-    env_only.env().contains_exactly({"BAR": "bar"})
+    env_only.env().entries().contains_exactly({"BAR": "bar"})
     env_only.files().contains_exactly(_SIMPLE_FILES)
 
     c_compile = env.expect.that_target(targets.simple).provider(ArgsListInfo).by_action().get(
@@ -110,6 +110,45 @@ def _env_only_test(env, targets):
 
     converted.flag_sets().contains_exactly([])
 
+def _env_only_requires_test(env, targets):
+    env_only = env.expect.that_target(targets.env_only_requires).provider(ArgsInfo)
+    env_only.actions().contains_at_least([
+        Label("//cc/toolchains/actions:c_compile"),
+        Label("//cc/toolchains/actions:cpp_compile"),
+    ])
+    env_only.env().entries().contains_exactly(
+        {"BAR": "%{dependency_file}"},
+    )
+
+    converted = env.expect.that_value(
+        convert_args(targets.env_only_requires[ArgsInfo]),
+        factory = _CONVERTED_ARGS,
+    )
+
+    converted.env_sets().contains_exactly([env_set(
+        actions = [
+            "assemble",
+            "c++-compile",
+            "c++-header-parsing",
+            "c++-module-codegen",
+            "c++-module-compile",
+            "c-compile",
+            "clif-match",
+            "linkstamp-compile",
+            "lto-backend",
+            "objc++-compile",
+            "objc-compile",
+            "preprocess-assemble",
+        ],
+        env_entries = [env_entry(
+            key = "BAR",
+            value = "%{dependency_file}",
+            expand_if_available = "dependency_file",
+        )],
+    )])
+
+    converted.flag_sets().contains_exactly([])
+
 def _with_dir_test(env, targets):
     with_dir = env.expect.that_target(targets.with_dir).provider(ArgsInfo)
     with_dir.allowlist_include_directories().contains_exactly([_TOOL_DIRECTORY])
@@ -124,8 +163,11 @@ TARGETS = [
     ":simple",
     ":some_variable",
     ":env_only",
+    ":env_only_requires",
     ":with_dir",
     ":iterate_over_optional",
+    ":good_env_format",
+    ":good_env_format_optional",
     "//tests/rule_based_toolchain/actions:c_compile",
     "//tests/rule_based_toolchain/actions:cpp_compile",
     "//tests/rule_based_toolchain/testdata:directory",
@@ -203,12 +245,11 @@ def _format_dict_values_test(env, targets):
     ])
     res.used_items().contains_exactly(["bar", "quuz", "qux"])
 
-    expected_label = Label("//tests/rule_based_toolchain/args:some_variable")
-    res = _expect_that_formatted(
+    _expect_that_formatted(
         env,
         {"foo": "{bar}"},
         {"bar": targets.some_variable},
-    ).err().equals("Unsupported cc_variable substitution " + str(expected_label) + ' in "{bar}".')
+    ).ok()
 
     _expect_that_formatted(
         env,
@@ -241,10 +282,73 @@ def _format_dict_values_test(env, targets):
         must_use = ["var"],
     ).err().contains('"var" was not used')
 
+def _good_env_format_test(env, targets):
+    good_env = env.expect.that_target(targets.good_env_format).provider(ArgsInfo)
+    good_env.env().entries().contains_exactly({"FOO": "%{gcov_gcno_file}"})
+
+    converted = env.expect.that_value(
+        convert_args(targets.good_env_format[ArgsInfo]),
+        factory = _CONVERTED_ARGS,
+    )
+    converted.env_sets().contains_exactly([env_set(
+        actions = [
+            "assemble",
+            "c++-compile",
+            "c++-header-parsing",
+            "c++-module-codegen",
+            "c++-module-compile",
+            "c-compile",
+            "clif-match",
+            "linkstamp-compile",
+            "lto-backend",
+            "objc++-compile",
+            "objc-compile",
+            "preprocess-assemble",
+        ],
+        env_entries = [env_entry(
+            key = "FOO",
+            value = "%{gcov_gcno_file}",
+        )],
+    )])
+
+def _good_env_format_optional_test(env, targets):
+    """Test that env formatting works with optional types."""
+    good_env_optional = env.expect.that_target(targets.good_env_format_optional).provider(ArgsInfo)
+    good_env_optional.env().entries().contains_exactly({"FOO": "%{dependency_file}"})
+
+    converted = env.expect.that_value(
+        convert_args(targets.good_env_format_optional[ArgsInfo]),
+        factory = _CONVERTED_ARGS,
+    )
+    converted.env_sets().contains_exactly([env_set(
+        actions = [
+            "assemble",
+            "c++-compile",
+            "c++-header-parsing",
+            "c++-module-codegen",
+            "c++-module-compile",
+            "c-compile",
+            "clif-match",
+            "linkstamp-compile",
+            "lto-backend",
+            "objc++-compile",
+            "objc-compile",
+            "preprocess-assemble",
+        ],
+        env_entries = [env_entry(
+            key = "FOO",
+            value = "%{dependency_file}",
+            expand_if_available = "dependency_file",
+        )],
+    )])
+
 # @unsorted-dict-items
 TESTS = {
     "simple_test": _simple_test,
     "format_dict_values_test": _format_dict_values_test,
     "env_only_test": _env_only_test,
+    "env_only_requires_test": _env_only_requires_test,
     "with_dir_test": _with_dir_test,
+    "good_env_format_test": _good_env_format_test,
+    "good_env_format_optional_test": _good_env_format_optional_test,
 }

cc/toolchains/cc_toolchain_info.bzl
@@ -97,6 +97,16 @@ NestedArgsInfo = provider(
     },
 )
 
+EnvInfo = provider(
+    doc = "A set of environment variables to be added to the environment for specific actions",
+    # @unsorted-dict-items
+    fields = {
+        "label": "(Label) The label defining this provider. Place in error messages to simplify debugging",
+        "entries": "(dict[str, str]) A mapping from environment variable name to value",
+        "requires_not_none": "(Optional[str]) The variable that must be not None to apply these environment variables",
+    },
+)
+
 ArgsInfo = provider(
     doc = "A set of arguments to be added to the command line for specific actions",
     # @unsorted-dict-items
@@ -106,7 +116,7 @@ ArgsInfo = provider(
         "requires_any_of": "(Sequence[FeatureConstraintInfo]) This will be enabled if any of the listed predicates are met. Equivalent to with_features",
         "nested": "(Optional[NestedArgsInfo]) The args expand. Equivalent to a flag group.",
         "files": "(depset[File]) Files required for the args",
-        "env": "(dict[str, str]) Environment variables to apply",
+        "env": "(EnvInfo) Environment variables to apply",
         "allowlist_include_directories": "(depset[DirectoryInfo]) Include directories implied by these arguments that should be allowlisted in Bazel's include checker",
         "allowlist_absolute_include_directories": "(depset[str]) Absolute include directories implied by these arguments that should be allowlisted in Bazel's include checker",
     },

cc/toolchains/impl/args_utils.bzl
@@ -122,3 +122,60 @@ def validate_nested_args(*, nested_args, variables, actions, label, fail = fail)
 
         for child in nested_args.nested:
             stack.append((child, overrides))
+
+def validate_env_variables(*, env, variables, actions, used_format_vars, fail = fail):
+    """Validates the typing for environment variables with requires_not_none and format variables.
+
+    Args:
+        env: (EnvInfo) The env info to validate
+        variables: (Dict[str, VariableInfo]) A mapping from variable name to
+          the VariableInfo.
+        actions: (List[ActionTypeInfo]) The actions we require these variables
+          to be valid for.
+        used_format_vars: (List[str]) The list of variables used in env format strings.
+        fail: The fail function. Use for testing only.
+    """
+    if not env.entries:
+        return
+
+    env_requires_types = {}
+    if env.requires_not_none:
+        env_requires_types[env.requires_not_none] = struct(
+            msg = "requires_not_none for env variables only works on optional string, file, or directory types",
+            valid_types = ["option"],
+            valid_elements = ["string", "file", "directory"],
+        )
+
+    for var_name in used_format_vars:
+        env_requires_types[var_name] = struct(
+            msg = "format only works on string, file, or directory type variables",
+            valid_types = ["string", "file", "directory", "option"],
+            valid_elements = ["string", "file", "directory"],
+        )
+
+    for var_name, requirement in env_requires_types.items():
+        type = get_type(
+            name = var_name,
+            variables = variables,
+            overrides = {},
+            actions = actions,
+            args_label = env.label,
+            nested_label = env.label,
+            fail = fail,
+        )
+
+        if type["name"] not in requirement.valid_types:
+            fail("{msg}, but {var_name} has type {type}".format(
+                var_name = var_name,
+                msg = requirement.msg,
+                type = type["repr"],
+            ))
+
+        if type["name"] == "option":
+            underlying_type = type["elements"]
+            if underlying_type["name"] not in requirement.valid_elements:
+                fail("{msg}, but {var_name} has type {type}".format(
+                    var_name = var_name,
+                    msg = requirement.msg,
+                    type = underlying_type["repr"],
+                ))

cc/toolchains/impl/legacy_converter.bzl
@@ -70,7 +70,11 @@ def convert_args(args, strip_actions = False):
         ))
 
     env_sets = []
-    if args.env:
+    if args.env.entries:
+        # NOTE: Use kwargs to support older bazel versions
+        kwargs = {}
+        if args.env.requires_not_none:
+            kwargs["expand_if_available"] = args.env.requires_not_none
         env_sets.append(legacy_env_set(
             actions = actions,
             with_features = with_features,
@@ -78,8 +82,9 @@ def convert_args(args, strip_actions = False):
                 legacy_env_entry(
                     key = key,
                     value = value,
+                    **kwargs
                 )
-                for key, value in args.env.items()
+                for key, value in args.env.entries.items()
             ],
         ))
     return struct(

cc/toolchains/impl/nested_args.bzl
@@ -291,10 +291,8 @@ def nested_args_provider(
 def _escape(s):
     return s.replace("%", "%%")
 
-def _format_target(target, arg, allow_variables, fail = fail):
+def _format_target(target, fail = fail):
     if VariableInfo in target:
-        if not allow_variables:
-            fail("Unsupported cc_variable substitution %s in %r." % (target.label, arg))
         return "%%{%s}" % target[VariableInfo].name
     elif DirectoryInfo in target:
         return _escape(target[DirectoryInfo].path)
@@ -305,7 +303,7 @@ def _format_target(target, arg, allow_variables, fail = fail):
 
     fail("%s should be either a variable, a directory, or a single file." % target.label)
 
-def _format_string(arg, format, used_vars, allow_variables, fail = fail):
+def _format_string(arg, format, used_vars, fail = fail):
     upto = 0
     out = []
     has_format = False
@@ -333,7 +331,7 @@ def _format_string(arg, format, used_vars, allow_variables, fail = fail):
             else:
                 used_vars[variable] = None
                 has_format = True
-                out.append(_format_target(format[variable], arg, allow_variables, fail = fail))
+                out.append(_format_target(format[variable], fail = fail))
                 upto += len(variable) + 2
 
         elif arg[upto] == "}":
@@ -363,7 +361,7 @@ def format_list(args, format, must_use = [], fail = fail):
     used_vars = {}
 
     for arg in args:
-        formatted.append(_format_string(arg, format, used_vars, True, fail))
+        formatted.append(_format_string(arg, format, used_vars, fail))
 
     unused_vars = [var for var in must_use if var not in used_vars]
     if unused_vars:
@@ -390,7 +388,7 @@ def format_dict_values(env, format, must_use = [], fail = fail):
     used_vars = {}
 
     for key, value in env.items():
-        formatted[key] = _format_string(value, format, used_vars, False, fail)
+        formatted[key] = _format_string(value, format, used_vars, fail)
 
     unused_vars = [var for var in must_use if var not in used_vars]
     if unused_vars:

tests/rule_based_toolchain/subjects.bzl
@@ -21,6 +21,7 @@ load(
     "ActionTypeSetInfo",
     "ArgsInfo",
     "ArgsListInfo",
+    "EnvInfo",
     "FeatureConstraintInfo",
     "FeatureInfo",
     "FeatureSetInfo",
@@ -138,12 +139,22 @@ _NestedArgsFactory = generate_factory(
 )
 
 # buildifier: disable=name-conventions
+_EnvInfoFactory = generate_factory(
+    EnvInfo,
+    "EnvInfo",
+    dict(
+        entries = _subjects.dict,
+        requires_not_none = optional_subject(_subjects.str),
+    ),
+)
+
+# buildifier: disable=name-conventions
 _ArgsFactory = generate_factory(
     ArgsInfo,
     "ArgsInfo",
     dict(
         actions = ProviderDepset(_ActionTypeFactory),
-        env = _subjects.dict,
+        env = _EnvInfoFactory.factory,
         files = _subjects.depset_file,
         # Use .factory so it's not inlined.
         nested = optional_subject(_NestedArgsFactory.factory),

commit 59cd38d0a6469fbecff8f1d35a64e384f7d7f853

Fix env var data substitutions
This feature was added in c93d01dee9102a64b43b4fc1f24c05fe3a5f4a37 and I
broke it in db1d07d78987ac9e996d8c0ca1f1b4b87554ee51 because the
validation was too aggressive and assumed every replacement was a
variable when they can actually be arbitrary strings pointing to files /
directories.

cc/toolchains/args.bzl
@@ -44,11 +44,19 @@ visibility("public")
 def _cc_args_impl(ctx):
     actions = collect_action_types(ctx.attr.actions)
 
+    format_targets = {k: v for v, k in ctx.attr.format.items()}
     formatted_env, used_format_vars = format_dict_values(
         env = ctx.attr.env,
         must_use = [],  # checking for unused variables in done when formatting `args`.
-        format = {k: v for v, k in ctx.attr.format.items()},
+        format = format_targets,
     )
+
+    # Ignore file / directory replacements for validation
+    used_format_vars = [
+        v
+        for v in used_format_vars
+        if v in format_targets and VariableInfo in format_targets[v]
+    ]
 
     for path in ctx.attr.allowlist_absolute_include_directories:
         if not is_path_absolute(path):

tests/rule_based_toolchain/args/BUILD
@@ -134,6 +134,30 @@ expect_failure_test(
     target = ":bad_env_format_wrong_action",
 )
 
+genrule(
+    name = "generated_file",
+    outs = ["generated_file.txt"],
+    cmd = "echo 'generated content' > $@",
+)
+
+util.helper_target(
+    cc_args,
+    name = "genrule_data_with_env_format",
+    actions = ["//tests/rule_based_toolchain/actions:all_compile"],
+    data = [":generated_file"],
+    env = {"GENERATED_PATH": "{generated_file}"},
+    format = {"generated_file": ":generated_file"},
+)
+
+util.helper_target(
+    cc_args,
+    name = "directory_format_in_args",
+    actions = ["//tests/rule_based_toolchain/actions:all_compile"],
+    args = ["-resource-dir={resource_dir}"],
+    data = ["//tests/rule_based_toolchain/testdata:directory"],
+    format = {"resource_dir": "//tests/rule_based_toolchain/testdata:directory"},
+)
+
 analysis_test_suite(
     name = "test_suite",
     targets = TARGETS,

tests/rule_based_toolchain/args/args_test.bzl
@@ -169,6 +169,22 @@ def _with_dir_and_data_test(env, targets):
     )
     c_compile.files().contains_at_least(_SIMPLE_FILES)
 
+def _directory_format_in_args_test(env, targets):
+    dir_args = env.expect.that_target(targets.directory_format_in_args).provider(ArgsInfo)
+    dir_args.actions().contains_exactly([
+        targets.c_compile.label,
+        targets.cpp_compile.label,
+    ])
+
+    converted = env.expect.that_value(
+        convert_args(targets.directory_format_in_args[ArgsInfo]),
+        factory = _CONVERTED_ARGS,
+    )
+    converted.flag_sets().contains_exactly([flag_set(
+        actions = ["c_compile", "cpp_compile"],
+        flag_groups = [flag_group(flags = ["-resource-dir=" + targets.directory[DirectoryInfo].path])],
+    )])
+
 TARGETS = [
     ":simple",
     ":some_variable",
@@ -179,6 +195,9 @@ TARGETS = [
     ":iterate_over_optional",
     ":good_env_format",
     ":good_env_format_optional",
+    ":genrule_data_with_env_format",
+    ":generated_file",
+    ":directory_format_in_args",
     "//tests/rule_based_toolchain/actions:c_compile",
     "//tests/rule_based_toolchain/actions:cpp_compile",
     "//tests/rule_based_toolchain/testdata:directory",
@@ -293,6 +312,17 @@ def _format_dict_values_test(env, targets):
         must_use = ["var"],
     ).err().contains('"var" was not used')
 
+def _genrule_data_with_env_format_test(env, targets):
+    genrule_args = env.expect.that_target(targets.genrule_data_with_env_format).provider(ArgsInfo)
+    genrule_args.actions().contains_exactly([
+        targets.c_compile.label,
+        targets.cpp_compile.label,
+    ])
+    genrule_args.env().entries().contains_exactly({
+        "GENERATED_PATH": targets.generated_file[DefaultInfo].files.to_list()[0].path,
+    })
+    genrule_args.files().contains_exactly(["tests/rule_based_toolchain/args/generated_file.txt"])
+
 def _good_env_format_test(env, targets):
     good_env = env.expect.that_target(targets.good_env_format).provider(ArgsInfo)
     good_env.env().entries().contains_exactly({"FOO": "%{gcov_gcno_file}"})
@@ -361,6 +391,8 @@ TESTS = {
     "env_only_requires_test": _env_only_requires_test,
     "with_dir_test": _with_dir_test,
     "with_dir_and_data_test": _with_dir_and_data_test,
+    "genrule_data_with_env_format_test": _genrule_data_with_env_format_test,
+    "directory_format_in_args_test": _directory_format_in_args_test,
     "good_env_format_test": _good_env_format_test,
     "good_env_format_optional_test": _good_env_format_optional_test,
 }