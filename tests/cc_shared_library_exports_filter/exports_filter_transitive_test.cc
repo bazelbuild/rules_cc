44eff68ca4af77676be573edaffa3b7c84b819c8
Cameron Martin <cameronmartin123@gmail.com>
2026-04-21 00:00:26 +0100

    Revert "Apply dllimport"
    This reverts commit 682ac95fa30642fe93c83e8f524f830dfe7d2ddb.

--- tests/cc_shared_library_exports_filter/exports_filter_transitive_test.cc
+++ tests/cc_shared_library_exports_filter/exports_filter_transitive_test.cc
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "uses_exported_transitive.h"
+int uses_exported_transitive();
 
 int main() { return uses_exported_transitive() == 7 ? 0 : 1; }
